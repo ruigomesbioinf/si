73dc2a4b6f4b517f230a7f60dd7aa008acf896ae
ruigomesbioinf <rfpgomes99@gmail.com>
2022-09-26 11:44:23 +0100

    [UPDATE] author, credits, maintainer, email

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,6 +1,6 @@
-__author__ = "Fernando Cruz"
-__credits__ = ["Vítor Pereira, Fernando Cruz"]
+__author__ = "Rui Gomes"
+__credits__ = ["Rui Gomes"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "Fernando Cruz"
-__email__ = "fernando.cruz@ceb.uminho.pt"
+__maintainer__ = "Rui Gomes"
+__email__ = "rfpgomes99@gmail.com"

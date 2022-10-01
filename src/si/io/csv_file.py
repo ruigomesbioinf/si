45f6d369268afdc3d75bba35f4a371207ea27685
ruigomesbioinf <rfpgomes99@gmail.com>
2022-10-01 17:49:18 +0100

    [ADD] add final read_csv and write_csv function for the io module

--- src/si/io/csv_file.py
+++ src/si/io/csv_file.py
@@ -4,28 +4,68 @@
 # Created Date: 26-09-2022
 # ---------------------------
 
-from typing import Optional
+import sys
+sys.path.insert(0, '/home/rui/Desktop/SIB/si/src/si')
+
+from typing import Optional, Union
 import pandas as pd
 import numpy as np
 
-from ..data.dataset import Dataset
+from data.dataset import Dataset
 
-def read_csv(filename:str, sep:str, features: Optional[bool] = True, label: Optional[bool] = True):
+def read_csv(filename:str, sep:str = ",", features: Optional[bool] = True, label: Union[None, int]= None):
     """Function that reads csv file and returns a Dataset object of that file.
 
     Args:
         filename (str): name/path of file
-        sep (str): separator between values
+        sep (str): separator between values. Defaults to , .
         features (Optional[bool], optional): If the csv file has feature names. Defaults to True.
-        label (Optional[bool], optional): If label names exist. Defaults to True.
+        label (int): If the dataset has defined labels receives an integer value that tells the column of the labels. Defaults to None.
     """
-    df = pd.read_csv(filename, sep = sep)
-    return Dataset(X=df.to_numpy(), y=np.arange(0, len(df)), features=list(df.columns), label = None)
+    imported_data = pd.read_csv(filepath_or_buffer=filename, sep=sep)
+    data = imported_data.values.tolist()
+    headers = list(imported_data.columns)
+    header_label = headers[label]
     
-def write_csv(filename:str, dataset, sep:str, features: bool, label: bool):
-    pass
+    if features:
+        if label is not None:
+            del headers[label]
+    else:
+        headers = None
+    
+    if label is not None:
+        y = list(imported_data.iloc[:, label])
+        imported_data = imported_data.drop(imported_data.columns[label], axis=1)
+        data = imported_data.values.tolist()
+    else: y = None
+    
+    return Dataset(X=data, y=y, features = headers, label = header_label)
+        
+    
+def write_csv(dataset, filename: str, sep: str = ",", features: Optional[bool] = True, label: Optional[bool] = True):
+    """Writes a csv file from a dataset object
+
+    Args:
+        dataset (_type_): Dataset to save on csv format
+        filename (str): Name of the csv file that will be saved
+        sep (str, optional): Separator of values. Defaults to ",".
+        features (Optional[bool], optional): Boolean value that tells if the dataset object has feature names. Defaults to True.
+        label (Optional[bool], optional): Boolean value that tells if the dataset object has label names Defaults to True.
+    """
+    csv = pd.DataFrame(data=dataset.X)
+    
+    if features:
+        csv.columns = dataset.features
+    
+    if label:
+        csv.insert(loc=0, column=dataset.label, value=dataset.y)
+        
+    csv.to_csv(filename, index=False)
+    
     
 if __name__ == "__main__":
     file = r"/home/rui/Desktop/SIB/si/datasets/iris.csv"
-    a = read_csv(file, sep=",")
-    print(a.print_dataframe())
+    a = read_csv(filename=file, sep = ",", features=True, label=4)
+    # print(a.print_dataframe())
+    # print(a.summary())
+    write_csv(a, "csv_write_test1.csv", features=True, label=False)

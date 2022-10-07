9389bb710b7c21d4aaff7cddcf028004472e6a0d
ruigomesbioinf <rfpgomes99@gmail.com>
2022-10-07 18:06:54 +0100

    [ADD] implemented select percentile as a feature selection option

---
+++ src/si/feature_selection/select_percentile.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+# ---------------------------
+# Created By  : Rui Gomes
+# Created Date: 05-10-2022
+# ---------------------------
+
+import sys
+sys.path.insert(0, 'src/si')
+# print(sys.path)
+
+from typing import Callable
+from statistic.f_classification import f_classification
+from data.dataset import Dataset
+import numpy as np
+
+
+class SelectPercentile:
+    
+    def __init__(self, percentile: float = 0.25, score_func: Callable = f_classification) -> None:
+        """Select the highest scoring features according to the percentile given.
+            The scores are computed using ANOVA F-values between label/feature.
+
+        Args:
+            percentile (float, optional): Percentile of features to select from all the features. Defaults to 0.25.
+            score_func (Callable, optional): Function that takes a dataset and returns the scores and p-values. Defaults to f_classification.
+
+        Raises:
+            ValueError: Raises ValueError if percentile isn't between 0 and 1.
+        """
+        
+        if percentile > 1 or percentile < 0:
+            raise ValueError("Your percentile must be a float between 0 and 1")
+        
+        self.score_func = score_func
+        self.percentile = percentile
+        self.F = None
+        self.p = None
+        
+    def fit(self, dataset: Dataset) -> "SelectPercentile":
+        """Fits SelectPercentile to compute the F scores and p_values.
+
+        Args:
+            dataset (Dataset): A dataset object
+
+        Returns:
+            SelectKBest: returns self
+        """
+        self.F, self.p = self.score_func(dataset)
+        return self
+    
+    def transform(self, dataset: Dataset) -> Dataset:
+        """It transforms the dataset by selecting the highest scores according to the percentile.
+
+        Args:
+            dataset (Dataset): A dataset object
+
+        Returns:
+            Dataset: A dataset object with the percentile highest score features.
+        """
+        len_features = len(dataset.features)
+        percentile = int(len_features * self.percentile)
+        idxs = np.argsort(self.F)[-percentile:]
+        features = np.array(dataset.features)[idxs]
+        return Dataset(X=dataset.X[:, idxs], y=dataset.y, features=list(features), label=dataset.label)
+    
+    def fit_transform(self, dataset: Dataset) -> Dataset:
+        """It runs the fit and the transform methods of this class automatically.
+
+        Args:
+            dataset (Dataset): A dataset object
+
+        Returns:
+            Dataset: A dataset object with the percentile highest score features.
+        """
+        self.fit(dataset)
+        return self.transform(dataset)
+    
+    
+
+if __name__ == "__main__":
+    a = SelectPercentile(0.75)
+    dataset = Dataset(X=np.array([[0, 2, 0, 3],
+                                  [0, 1, 4, 3],
+                                  [0, 1, 1, 3]]),
+                      y=np.array([0, 1, 0]),
+                      features=["f1", "f2", "f3", "f4"],
+                      label="y")
+    a.fit(dataset)
+    b = a.transform(dataset)
+    print(b.features)  
+    

798da9bd1475abf718867d4668e561604c0c50ac
ruigomesbioinf <rfpgomes99@gmail.com>
2022-10-17 17:56:41 +0100

    [UPDATE] Commenting

--- src/si/statistic/euclidean_distance.py
+++ src/si/statistic/euclidean_distance.py
@@ -13,8 +13,11 @@ import numpy as np
 def euclidean_distance(x: np.ndarray, y: np.ndarray) -> np.ndarray:
     """It computes the euclidean distance of a point(x) to a set of points y.
 
+    distance_y1n = sqrt((x1 - y11)^2 + (x2 - y12)^2) + ... + sqrt((xn - y1n)^2)
+    distance_y2n = sqrt((x1 - y21)^2 + (x2 - y22)^2) + ... + sqrt((xn - y2n)^2)  
+    
     Args:
-        x (np.ndarray): Point.
+        x (np.ndarray): Vector of points.
         y (np.ndarray): Set of points
 
     Returns:

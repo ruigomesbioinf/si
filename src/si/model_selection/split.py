92214ae2ba2e9c87582d8ae157c371001c1a4afb
ruigomesbioinf <rfpgomes99@gmail.com>
2022-11-05 17:24:36 +0000

    [FIX] bugs concerning returns

--- src/si/model_selection/split.py
+++ src/si/model_selection/split.py
@@ -5,13 +5,17 @@
 # ---------------------------
 
 import sys
+import os
 sys.path.insert(0, 'src/si')
 # print(sys.path)
+print(os.getcwd())
 
 from data.dataset import Dataset
+from IO.csv_file import read_csv
+
 import numpy as np
 
-def train_test_split (dataset: Dataset, test_size: float = 0.2, random_state: int = 42) -> tuple:
+def train_test_split (dataset: Dataset, test_size: float = 0.2, random_state: int = 42) -> tuple[Dataset, Dataset]:
     """
     Random splits a dataset into a train and a test set.
 
@@ -27,6 +31,9 @@ def train_test_split (dataset: Dataset, test_size: float = 0.2, random_state: in
     permutations = np.random.permutation(len_samples)
     test_split = permutations[:len_test]
     train_split = permutations[len_test:]
-    train = Dataset(X=dataset.X[train_split], y=dataset.y[train_split], features=dataset.features, label=dataset.label)
-    test = Dataset(X=dataset.X[test_split], y=dataset.y[test_split], features=dataset.features, label=dataset.label)
+    train = Dataset(dataset.X[train_split], dataset.y[train_split], features = dataset.features,
+                    label = dataset.label)
+
+    test = Dataset(dataset.X[test_split], dataset.y[test_split], features = dataset.features,
+                   label = dataset.label)
     return train, test

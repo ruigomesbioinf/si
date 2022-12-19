448ec49ae401251faaa12a0f043a2c59e5fed868
ruigomesbioinf <rfpgomes99@gmail.com>
2022-12-19 11:31:31 +0000

    [ADD] implementation of sigmoid activation class

---
+++ src/si/neural_networks/sigmoid_activation.py
@@ -0,0 +1,39 @@
+import sys
+sys.path.insert(0, 'src/si')
+# print(sys.path)
+
+import numpy as np
+from statistic.sigmoid_function import sigmoid_function
+
+class SigmoidActivation:
+    def __init__(self):
+        # attributes
+        self.input_data = None
+        
+    def forward(self, input_data: np.ndarray) -> np.ndarray:
+        """
+        Computes the forward pass of the layer.
+
+        Args:
+            input_data (np.ndarray): Input data.
+        """
+        self.input_data = input_data
+        return sigmoid_function(input_data)
+    
+    def backward(self, error: np.ndarray) -> np.ndarray:
+        """
+        Computes the backward pass of the layer.
+
+        Args:
+            error (np.ndarray): Error
+
+        Returns:
+            np.ndarray: Return the error of the previous layer.
+        """
+        
+        # multiplication by the derivative and not the entire matrix
+        sigmoid_deriv = sigmoid_function(self.input_data) * (1 - sigmoid_function(self.input_data))
+        
+        error_to_propagate = error * sigmoid_deriv
+        
+        return error_to_propagate

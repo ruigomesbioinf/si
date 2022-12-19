c48fb05926a66633afc1f3d144a373945d7ebcb7
ruigomesbioinf <rfpgomes99@gmail.com>
2022-12-19 11:31:06 +0000

    [ADD] implementation of rectified linear unit deep learning

---
+++ src/si/neural_networks/relu_activation.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.insert(0, 'src/si')
+# print(sys.path)
+
+
+import numpy as np
+
+class ReLUActivation:
+    def __init__(self) -> None:
+        self.input_data = None
+        
+    def forward(self, input_data: np.ndarray):
+        """
+        Computes the rectified linear unit relationship
+
+        Args:
+            input_data (np.ndarray): Input data
+        """
+        self.input_data = input_data
+        
+        return np.maximum(0, input_data) # 0 arg to avoi negative values
+    
+    def backward(self, error: np.ndarray) -> np.ndarray:
+        """
+        Computes teh backward pass of the rectified linear unit relationshop.
+
+        Args:
+            error (np.ndarray): Error
+
+        Returns:
+            np.ndarray: Error of the previous layer.
+        """
+        
+        relu_deriv = np.where(self.input_data > 0, 1, 0)
+        
+        error_to_propagate = error * relu_deriv
+        
+        return error_to_propagate
